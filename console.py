b2885dfb8b7abbdc975524a618b8d168866d3d5b
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-05 14:04:14 +0000

    Added create method to console class

--- console.py
+++ console.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 import cmd
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    all_classes = {
+        'BaseModel': BaseModel
+    }
+
+    def do_create(self, arg):
+        """Creates a new model instance of the class passed as arg
+
+            Usage: create <ModelName>
+        """
+        if not arg:
+            print('** class name missing **')
+            return
+        try:
+            new_instance = HBNBCommand.all_classes[arg]()
+            print(new_instance.id)
+        except KeyError:
+            print("** class doesn't exist **")
 
     def do_EOF(self, line):
         """Handle End-of-File (EOF) input"""
@@ -14,7 +33,7 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         """An empty line + `ENTER` shouldn’t execute anything"""
-        pass
+
 
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()    
+    HBNBCommand().cmdloop()
