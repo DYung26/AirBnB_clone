ce49fedbb165539643497e0a3d6fbac4ac9dd948
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-05 14:34:33 +0000

    Added do_destroy method to the console

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -9,19 +10,45 @@ class HBNBCommand(cmd.Cmd):
         'BaseModel': BaseModel
     }
 
-    def do_create(self, arg):
+    def do_create(self, line):
         """Creates a new model instance of the class passed as arg
 
             Usage: create <ModelName>
         """
-        if not arg:
+        if not line:
             print('** class name missing **')
             return
         try:
-            new_instance = HBNBCommand.all_classes[arg]()
+            new_instance = HBNBCommand.all_classes[line]()
             print(new_instance.id)
         except KeyError:
             print("** class doesn't exist **")
+
+    def do_destroy(self, line):
+        """do_destroy deletes a model instance
+
+            Usage: destroy <ModelName> <ModelId>
+        Args:
+            line (str): model name and model id
+        """
+        args = line.split()
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        model_name = args[0]
+        if model_name not in HBNBCommand.all_classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        model_id = args[1]
+        key = model_name + "." + model_id
+        all_objs = storage.all()
+        if key not in all_objs:
+            print("** no instance found **")
+            return
+        del all_objs[key]
 
     def do_EOF(self, line):
         """Handle End-of-File (EOF) input"""
