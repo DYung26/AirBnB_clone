e0328ca20c7d25f201b3977539c0a0d4a9e2a84e
DYung26 <theadstore1407@gmail.com>
2024-03-05 05:09:12 -0800

    Implemented `quit` and `EOF` in command interpreter to exit the program.

--- console.py
+++ console.py
@@ -3,8 +3,11 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 
+    def do_EOF(self, line):
+        return True
+
     def do_quit(self, line):
-        print("quit")
+        return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()    
