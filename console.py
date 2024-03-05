bfba54e5318f763e36ac73d3c1d8182f047a0ca7
DYung26 <theadstore1407@gmail.com>
2024-03-05 05:46:16 -0800

    A program called `console.py` that contains the entry point of the command interpreter.

--- console.py
+++ console.py
@@ -5,10 +5,16 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_EOF(self, line):
+        """Handle End-of-File (EOF) input"""
         return True
 
     def do_quit(self, line):
+        """Quit command to exit the program"""
         return True
+
+    def emptyline(self):
+        """An empty line + `ENTER` shouldn’t execute anything"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()    
