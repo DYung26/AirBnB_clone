62723873ad874b6ec20ddb84512fb349c00d1381
DYung26 <theadstore1407@gmail.com>
2024-03-08 04:34:40 -0800

    A class `User` that inherits from `BaseModel`

---
+++ models/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module: user.py
+This module defines the User class.
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class which inherits from BaseModel.
+
+    Class Attributes:
+        email (str): The email address of the user.
+        password (str): The password associated with the user's account.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+    email: ""
+    password: ""
+    first_name: ""
+    last_name: ""
