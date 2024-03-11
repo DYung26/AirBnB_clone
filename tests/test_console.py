ab31d7919547f184eb47e988c14a415391667ec0
DYuŋ <80885248+DYung26@users.noreply.github.com>
2024-03-11 10:57:13 +0100

    Updated `test_console.py`

--- tests/test_console.py
+++ tests/test_console.py
@@ -1259,7 +1259,7 @@ class TestHBNBCommand_update(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as output:
             HBNBCommand().onecmd("create Amenity")
             testId = output.getvalue().strip()
-        testCmd = "update Amenity {} attr_name 'attr_value'".format(testId)
+        testCmd = "update Amenity {} attr_name attr_value".format(testId)
         self.assertFalse(HBNBCommand().onecmd(testCmd))
         test_dict = storage.all()["Amenity.{}".format(testId)].__dict__
         self.assertEqual("attr_value", test_dict["attr_name"])
