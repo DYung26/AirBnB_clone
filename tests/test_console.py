b185164140fab4550a7fbe31467ebdd3948a9dd8
Marzouq Adebayo <marzouqaadebayo@gmail.com>
2024-03-11 09:35:58 +0000

    Fixed error in test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -783,23 +783,25 @@ class TestHBNBCommand_all(unittest.TestCase):
             self.assertIn("Review", output.getvalue().strip())
 
     def test_all_objects_dot_notation(self):
+        models = [
+            "BaseModel",
+            "User",
+            "State",
+            "Place",
+            "City",
+            "Amenity",
+            "Review"
+        ]
+        for i, model in enumerate(models):
+            with patch("sys.stdout", new=StringIO()) as output:
+                HBNBCommand().onecmd("create " + model)
+                models[i] = (model, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("create BaseModel"))
-            self.assertFalse(HBNBCommand().onecmd("create User"))
-            self.assertFalse(HBNBCommand().onecmd("create State"))
-            self.assertFalse(HBNBCommand().onecmd("create Place"))
-            self.assertFalse(HBNBCommand().onecmd("create City"))
-            self.assertFalse(HBNBCommand().onecmd("create Amenity"))
-            self.assertFalse(HBNBCommand().onecmd("create Review"))
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd(".all()"))
-            self.assertIn("BaseModel", output.getvalue().strip())
-            self.assertIn("User", output.getvalue().strip())
-            self.assertIn("State", output.getvalue().strip())
-            self.assertIn("Place", output.getvalue().strip())
-            self.assertIn("City", output.getvalue().strip())
-            self.assertIn("Amenity", output.getvalue().strip())
-            self.assertIn("Review", output.getvalue().strip())
+            HBNBCommand().onecmd("all")
+            all_objs = output.getvalue().strip()
+        for model in models:
+            self.assertIn(model[0], all_objs)
+            self.assertIn(model[1], all_objs)
 
     def test_all_single_object_space_notation(self):
         with patch("sys.stdout", new=StringIO()) as output:
